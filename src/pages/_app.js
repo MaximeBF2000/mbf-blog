772d9e5075024995aed8b982293da3dc0f667c18
Maxime FERRET <maxime.bounaas@gmail.com>
2023-05-31 22:37:43 +0200

    setup vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router'
+import { Analytics } from '@vercel/analytics/react'
 import { Navigation } from 'src/components/Navigation.component'
 import { Footer } from 'src/components/Footer.component'
 import '../global.css'
@@ -8,12 +9,15 @@ export default function NextApp({ Component, pageProps }) {
   const path = asPath.split('#')[0].split('?')[0]
 
   return (
-    <div className="relative bg-gray-800 min-h-screen text-white pt-16 pb-48">
-      <div className="w-full md:w-2/3 lg:w-1/2 px-3 md:px-0 mx-auto">
-        <Navigation currentPath={path} />
-        <Component {...pageProps} />
+    <>
+      <Analytics />
+      <div className="relative bg-gray-800 min-h-screen text-white pt-16 pb-48">
+        <div className="w-full md:w-2/3 lg:w-1/2 px-3 md:px-0 mx-auto">
+          <Navigation currentPath={path} />
+          <Component {...pageProps} />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   )
 }
